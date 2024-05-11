bf69dbc14dd54d22db63d76bfe117c07f5ff0ec8
Vinh Nguyen <oturkeybeoo@gmail.com>
2024-05-11 16:38:16 +0700

    Done feedward

--- Labwork4/NeuralNetwork.py
+++ Labwork4/NeuralNetwork.py
@@ -27,13 +27,15 @@ class Link:
         self.weight = weight
 
 class Layer:
-    def __init__(self, neuron_no) -> None:
+    def __init__(self, activation, neuron_no) -> None:
+        self.activation = activation
         self.neurons = self._init_neurons(neuron_no)
 
     def _init_neurons(self, neuron_no):
         neurons = [BiasNeuron()]
         for _ in range(neuron_no):
-            neurons.append(Neuron())
+            neurons.append(Neuron(self.activation))
+        return neurons
 
     def output(self):
         pass
@@ -48,14 +50,15 @@ class LayerLink:
         links = []
         for fNeuron in self.fromLayer.neurons:
             for tNeuron in self.toLayer.neurons:
-                if (isinstance(tNeuron, BiasNeuron)): 
+                if (isinstance(tNeuron, BiasNeuron)):
                     pass
-                links.append(Link(fNeuron, tNeuron, random.random()))
+                links.append(Link(fNeuron, tNeuron, random.randrange(-100,100)/100))
         return links
 
 class NeuronNetwork:
-    def __init__(self, file) -> None:
+    def __init__(self, activation, file) -> None:
         layer_no, neuron_no = self._get_data(file)
+        self.activation = activation
         self.layers = self._init_layers(layer_no, neuron_no)
         self.layerLinks = self._init_layer_links()
 
@@ -65,27 +68,39 @@ class NeuronNetwork:
     def _init_layers(self, layer_no, neuron_no):
         layers = []
         for i in range(layer_no):
-            layers.append(Layer(neuron_no[i]))
+            layers.append(Layer(self.activation, neuron_no[i]))
         return layers
 
     def _init_layer_links(self):
         layerLinks = []
         for i in range(len(self.layers)-1):
-            layerLinks.append(self.layers[i], self.layers[i+1])
+            layerLinks.append(LayerLink(self.layers[i], self.layers[i+1]))
         return layerLinks
 
-    def input(self, inputs: list):
-        for i in inputs:
-            self.layers[0].neurons[i].output = inputs[i]
+    def predict(self, inputs):
+        input_layer = self.layers[0]
+        for i, neuron in enumerate(input_layer.neurons[1:]):
+            neuron.output = inputs[i]
 
-    def output(self):
-        for i in range(len(self.layers[1:])):
-            neuron_input = 0
-            for neuron in self.layers[i]:
-                for link in self.layerLinks[i]:
+        for i, layer in enumerate(self.layers[1:]):
+            for neuron in layer.neurons:
+                neuron_input = 0
+                for link in self.layerLinks[i].links:
                     if link.toNeuron == neuron:
-                        pass
+                        neuron_input += link.weight * link.fromNeuron.output
+                print(f"Before: {neuron.output}")
+                neuron.update(neuron_input)
+                print(f"After: {neuron.output}")
+        
+    def output(self):
+        for i in range(len(self.layers)):
+            for neuron in self.layers[i].neurons:
+                print(f"{i} layer: {neuron.output}")
 
 
 if __name__ == "__main__":
-    activation = lambda x: 1/(math.exp(-x))
+    activation = lambda x: 1/(1+math.exp(-x))
+
+    nn = NeuronNetwork(activation, "")
+    nn.predict([0,0])
+    nn.output()
